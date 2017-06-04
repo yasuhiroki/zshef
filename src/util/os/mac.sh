4c66be2ae762a3451d68fedab39695b1dc350183
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-06-04 22:47:36 +0900

    Add mac install/update util function

---
+++ src/util/os/mac.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+function _zshef::util::os::mac::install::cmd() {
+  echo "${ZSHEF_MAC_INSTALL_CMD:-brew}"
+}
+
+function zshef::util::os::mac::install() {
+  $(_zshef::util::os::mac::install::cmd) install $1
+}
+
+function zshef::util::os::mac::update() {
+  if $(_zshef::util::os::mac::install::cmd) outdated | grep -sq "^$1 "; then
+    $(_zshef::util::os::mac::install::cmd) upgrade $1
+  else
+    echo "$1 is already updated"
+  fi
+}
+
