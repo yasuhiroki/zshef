57588060f5c08d512d70e9055b0597dd5b8c0d81
yasuhiroki <yasuhiroki.duck@gmail.com>
2019-05-04 15:04:01 +0900

    Add golang install/update util function

---
+++ src/util/lang/go.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+function _zshef::util::lang::go::install::cmd() {
+    echo "${ZSHEF_GO_INSTALL_CMD:-go get}"
+}
+
+function zshef::util::lang::go::install() {
+    zshef::util::mng::is_installed "go" || {
+        zshef::util::log::error "required golang"
+        return 1
+    }
+
+    $(_zshef::util::lang::go::install::cmd) $@
+}
+
+function zshef::util::lang::go::update() {
+    zshef::util::lang::go::install -u "$1"
+}
