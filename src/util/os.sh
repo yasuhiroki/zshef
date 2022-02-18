66d70d81108c46e2ba42358ae671c5e349db0790
yasuhiroki <yasuhiroki.duck@gmail.com>
2022-02-19 02:11:52 +0900

    fix bug `command not found 'is'`

--- src/util/os.sh
+++ src/util/os.sh
@@ -14,9 +14,9 @@ function zshef::util::os::is_debian() {
 
 function zshef::util::os::install() {
   if zshef::util::os::is_osx; then
-    $(zshef::util::os::mac::install) $1
+    zshef::util::os::mac::install $1
   elif zshef::util::os::is_debian; then
-    $(zshef::util::os::debian::install) $1
+    zshef::util::os::debian::install $1
   else
     zshef::util::log::error 'Not supported'
   fi
@@ -24,9 +24,9 @@ function zshef::util::os::install() {
 
 function zshef::util::os::update() {
   if zshef::util::os::is_osx; then
-    $(zshef::util::os::mac::update) $1
+    zshef::util::os::mac::update $1
   elif zshef::util::os::is_debian; then
-    $(zshef::util::os::debian::update) $1
+    zshef::util::os::debian::update $1
   else
     zshef::util::log::error 'Not supported'
   fi
